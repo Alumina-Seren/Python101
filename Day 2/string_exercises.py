b77175a260b935d41b6d3b81be834203d4120ee6
Magdalena Kropidłowska <Magdalena@MacBook-Pro-Magdalena.local>
2019-06-26 20:18:04 +0200

    Initial commit

---
+++ Day 2/string_exercises.py
@@ -0,0 +1,19 @@
+messages = "Witaj, Jonie Snow"
+
+print(messages)
+
+imie = 'Magdalena' \
+
+nazwisko = 'K' \
+
+print(f'Witaj {imie} {nazwisko}')
+
+print(f'Witaj{imie}{nazwisko}')
+
+print('Witaj' + imie + nazwisko)
+
+print('Witaj' + 'Magdalena' + 'K')
+
+# komentarz
+
+
