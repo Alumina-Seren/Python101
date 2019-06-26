0c5c13f29f375d3b62865e9c7e11401b96a11a5c
Alumina-Seren <me.kropidlowska@gmail.com>
2019-06-26 23:35:27 +0200

    dodalam komentarz dodalam powtorzony material

--- Day 2/string_exercises.py
+++ Day 2/string_exercises.py
@@ -18,6 +18,34 @@ print('Witaj' + ' Magdalena' + ' K')
 
 # dodałam komentarz
 
+# Strings - powtorka
+print("Hello World")
+print('Hello World')
+print("Hello "*3)
+print("Hello World! " + "It's so warm outside")
+print("Mr Smith's book\n is incredible")
+print("Mr Smith's book is incredible")
+print("Mr Smith's book\n \t is incredible")
+print('Mr Smith\'s book is incredible')
 
+# Numbers - powtorka
+print(2+2)
+print(4-3)
+print(2*2)
+print(4/2)
+print(3/2)
+print(2**3)
+print(3%2)
+print(2//3)
+print(3//2)
+print(type(2))
+print(type(1.5))
+print(type('number'))
 
-
+# True/ False - powtorka
+print(True)
+print(False)
+print(2==2)
+print(2>3)
+print(1<4)
+print(3==3.0)
